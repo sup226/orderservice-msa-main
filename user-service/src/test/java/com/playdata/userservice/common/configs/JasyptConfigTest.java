8c75a3ac46b85a53f501b42d3db85b41942654f4
sup226 <rlaalswl1102@naver.com>
2024-11-27 10:41:23 +0900

    Jasypt를 이용한 특정 민감 정보 암호화 처리. (Spring Cloud Config와는 연관 없음)

--- user-service/src/test/java/com/playdata/userservice/common/configs/JasyptConfigTest.java
+++ user-service/src/test/java/com/playdata/userservice/common/configs/JasyptConfigTest.java
@@ -7,8 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @SpringBootTest
 class JasyptConfigTest {
 
